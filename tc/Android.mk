defd91fa4bdbaffc5fcd53ad4bb84d74ef2459fd
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2014-07-17 17:21:44 -0600

    tc: Add flow control setting on PRIO qdisc
    * Enable filter fw, qdiscs prio, and qdisc fifo.
    * Add "flow enable|disable" clause for PRIO qdisc
    
    CRs-Fixed: 2236623
    Change-Id: I7ccfe9717bf21738c66b33dbafe04b2c3a3a912a

--- tc/Android.mk
+++ tc/Android.mk
@@ -3,7 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  tc.c tc_exec.c tc_qdisc.c q_cbq.c tc_util.c tc_class.c tc_core.c m_action.c \
                     m_estimator.c tc_filter.c tc_monitor.c tc_stab.c tc_cbq.c \
-                    tc_estimator.c f_u32.c m_police.c q_ingress.c m_mirred.c q_htb.c
+                    tc_estimator.c f_u32.c m_police.c q_ingress.c m_mirred.c q_htb.c \
+                    f_fw.c q_prio.c q_fifo.c
 
 LOCAL_MODULE := tc
 
@@ -14,7 +15,9 @@ LOCAL_SHARED_LIBRARIES += libiprouteutil libnetlink
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include $(UAPI_INCLUDES)
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -Wno-pointer-arith -Wno-sign-compare -Werror \
+LOCAL_CFLAGS := -DFEATURE_PRIO
+
+LOCAL_CFLAGS += -O2 -g -W -Wall -Wno-pointer-arith -Wno-sign-compare -Werror \
     -Wno-unused-parameter \
     -Wno-missing-field-initializers
 
