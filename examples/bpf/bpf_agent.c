iproute2/iproute2

commit 88eea5395483bb26a3b5fbbad66334feeb1ec3b6

tc: {f,m}_bpf: allow to retrieve uds path from env
Allow to retrieve uds path from the environment, facilitates
also dealing with export a bit.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

tc/f_bpf.c
@@ -122,6 +122,7 @@ opt_bpf:
 
 			NEXT_ARG();
 			if (ebpf) {
+				bpf_uds_name = secure_getenv(BPF_ENV_UDS);
 				bpf_obj = *argv;
 				NEXT_ARG();
 
@@ -131,8 +132,9 @@ opt_bpf:
 					bpf_sec_name = *argv;
 					NEXT_ARG();
 				}
-				if (strcmp(*argv, "export") == 0 ||
-				    strcmp(*argv, "exp") == 0) {
+				if (!bpf_uds_name &&
+				    (strcmp(*argv, "export") == 0 ||
+				     strcmp(*argv, "exp") == 0)) {
 					NEXT_ARG();
 					bpf_uds_name = *argv;
 					NEXT_ARG();

tc/m_bpf.c
@@ -105,6 +105,7 @@ opt_bpf:
 
 			NEXT_ARG();
 			if (ebpf) {
+				bpf_uds_name = secure_getenv(BPF_ENV_UDS);
 				bpf_obj = *argv;
 				NEXT_ARG();
 
@@ -114,8 +115,9 @@ opt_bpf:
 					bpf_sec_name = *argv;
 					NEXT_ARG();
 				}
-				if (strcmp(*argv, "export") == 0 ||
-				    strcmp(*argv, "exp") == 0) {
+				if (!bpf_uds_name &&
+				    (strcmp(*argv, "export") == 0 ||
+				     strcmp(*argv, "exp") == 0)) {
 					NEXT_ARG();
 					bpf_uds_name = *argv;
 					NEXT_ARG();

tc/tc_bpf.h
@@ -25,6 +25,8 @@
 #include "utils.h"
 #include "bpf_scm.h"
 
+#define BPF_ENV_UDS	"TC_BPF_UDS"
+
 int bpf_parse_string(char *arg, bool from_file, __u16 *bpf_len,
 		     char **bpf_string, bool *need_release,
 		     const char separator);

MotorolaMobilityLLC/external-iproute2

commit 2bb890788b994947f539438a617d2b195a2f3a69

tc: fix bpf compilation with old glibc
Error was:
f_bpf.o: In function `bpf_parse_opt':
f_bpf.c:(.text+0x88f): undefined reference to `secure_getenv'
m_bpf.o: In function `parse_bpf':
m_bpf.c:(.text+0x587): undefined reference to `secure_getenv'
collect2: error: ld returned 1 exit status

There is no special reason to use the secure version of getenv, thus let's
simply use getenv().

CC: Daniel Borkmann <daniel@iogearbox.net>
Fixes: 88eea5395483 ("tc: {f,m}_bpf: allow to retrieve uds path from env")
Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
Acked-by: Daniel Borkmann <daniel@iogearbox.net>
Acked-by: Alexei Starovoitov <ast@plumgrid.com>
Tested-by: Yegor Yefremov <yegorslists@googlemail.com>

examples/bpf/bpf_agent.c
@@ -154,7 +154,7 @@ static void bpf_map_get_from_env(int *tfd)
 		memset(key, 0, sizeof(key));
 		snprintf(key, sizeof(key), "BPF_MAP%d", i);
 
-		val = secure_getenv(key);
+		val = getenv(key);
 		assert(val != NULL);
 
 		tfd[i] = atoi(val);

tc/f_bpf.c
@@ -122,7 +122,7 @@ opt_bpf:
 
 			NEXT_ARG();
 			if (ebpf) {
-				bpf_uds_name = secure_getenv(BPF_ENV_UDS);
+				bpf_uds_name = getenv(BPF_ENV_UDS);
 				bpf_obj = *argv;
 				NEXT_ARG();
 

tc/m_bpf.c
@@ -105,7 +105,7 @@ opt_bpf:
 
 			NEXT_ARG();
 			if (ebpf) {
-				bpf_uds_name = secure_getenv(BPF_ENV_UDS);
+				bpf_uds_name = getenv(BPF_ENV_UDS);
 				bpf_obj = *argv;
 				NEXT_ARG();
 